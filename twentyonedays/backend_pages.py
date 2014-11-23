1e19d52b64b07b15d64e22f0b80890977509db42
Lily Zhang <lilyzhang@Lilys-MacBook-Air.local>
2014-11-22 22:31:51 -0500

    adds project to settings and adds models

--- twentyonedays/backend_pages.py
+++ twentyonedays/backend_pages.py
@@ -1,9 +1,14 @@
 from django.http.response import HttpResponse
 from django.shortcuts import render_to_response
+from models import NewsFeedPost
+from django.contrib.auth.models import User
 
 
 def home(request):
-    return render_to_response("index.html")
+    context = {"users": User.objects.all(),
+               "news_feed_posts": NewsFeedPost.objects.all(),
+               }
+    return render_to_response("index.html", context)
 
 
 def users(request):
