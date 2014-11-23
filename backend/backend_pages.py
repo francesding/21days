ab4aaf29c583b4e8b03d4920f0cf1afa698d1ced
jimmy <Jimmy@Jimmys-MacBook-Pro-2.local>
2014-11-23 00:10:01 -0500

    loginhandler

--- backend/backend_pages.py
+++ backend/backend_pages.py
@@ -2,12 +2,15 @@ from django.http.response import HttpResponse
 from django.shortcuts import render_to_response
 from models import NewsFeedPost
 from django.contrib.auth.models import User
-
+from django.core.urlresolvers import reverse
+from django.contrib.auth import authenticate
+from django.core.context_processors import csrf
 
 def home(request):
     context = {"users": User.objects.all(),
                "news_feed_posts": NewsFeedPost.objects.all(),
                }
+    context.update(csrf(request))
     return render_to_response("index.html", context)
 
 
@@ -22,3 +25,20 @@ def habit(request):
 
 def login(request):
     return render_to_response("login.html")
+
+def loginhandler(request):
+    username = request.POST['username']
+    password = request.POST['password']
+    user = authenticate(username=username, password=password)
+    if user is not None:
+        if user.is_active:
+            login(request, user)
+            return reverse("home")
+            # Redirect to a success page.
+        else:
+            # Return a 'disabled account' error message
+            pass
+    else:
+        # Return an 'invalid login' error message.
+        return HttpResponse("This account does not exist! Create one here!")
+    
--- backend/urls.py
+++ backend/urls.py
@@ -1,13 +1,13 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
-from backend_pages import (users, home, habit, habit_input, login) #loginhandler)
+from backend_pages import (users, home, habit, habit_input, login, loginhandler)
 
 urlpatterns = patterns('',
     # Examples:
     url(r'^$', home, name='home'),
     url(r'^users$', users, name='users'),
     url(r'^login$', login, name='login'),
-    #url(r'^loginhandler$', loginhandler, name='loginhandler'),
+    url(r'^loginhandler$', loginhandler, name='loginhandler'),
     url(r'^habit_input$', habit_input, name = 'habit_input'),
     url(r'^habit$', habit, name = 'habit'),
     #url(r'^$register', register, name = 'register'),
